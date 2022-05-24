9f8708133bb5dc6402906482bcf945827fa94a5b
Zoha Jafri <zoha.jafarii4@gmail.com>
2022-05-24 12:40:32 +0430

    Updates

---
+++ README.md
@@ -0,0 +1,1 @@
+# Viuna Rating Form
---
+++ js/script.js
@@ -0,0 +1,20 @@
+var vote = 0;
+for(let star of document.getElementsByClassName('star')) {
+    star.addEventListener('click', (e) => {
+        vote = parseInt(e.target.id);
+        updateVote()
+    })
+
+}
+
+function updateVote(){
+    for(let star of document.getElementsByClassName('star')) {
+        if(parseInt(star.id) <= vote) {
+            star.classList.replace('bi-star', 'bi-star-fill')
+            star.classList.add('selected')
+        } else {
+            star.classList.replace('bi-star-fill', 'bi-star')
+            star.classList.remove('selected')
+        }
+    }
+}
