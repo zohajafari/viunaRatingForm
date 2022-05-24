6097d721e22654b1675854b93f0ae7de0ee9ade4
Zoha Jafri <zoha.jafarii4@gmail.com>
2022-05-25 00:40:27 +0430

    a welcome message added

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 var vote = 0;
-for(let star of document.getElementsByClassName('star')) {
-    star.addEventListener('click', (e) => {
-        vote = parseInt(e.target.id);
-        updateVote()
-    })
-
+for (let star of document.getElementsByClassName("star")) {
+  star.addEventListener("click", (e) => {
+    vote = parseInt(e.target.id);
+    updateVote();
+  });
 }
 
-function updateVote(){
-    for(let star of document.getElementsByClassName('star')) {
-        if(parseInt(star.id) <= vote) {
-            star.classList.replace('bi-star', 'bi-star-fill')
-            star.classList.add('selected')
-        } else {
-            star.classList.replace('bi-star-fill', 'bi-star')
-            star.classList.remove('selected')
-        }
+function updateVote() {
+  for (let star of document.getElementsByClassName("star")) {
+    if (parseInt(star.id) <= vote) {
+      star.classList.replace("bi-star", "bi-star-fill");
+      star.classList.add("selected");
+    } else {
+      star.classList.replace("bi-star-fill", "bi-star");
+      star.classList.remove("selected");
     }
+  }
 }
